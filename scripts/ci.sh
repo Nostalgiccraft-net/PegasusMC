1a4078991328bc4e5596355b53232529d317613f
bergschneider <magiclake@gmx.de>
2023-05-26 23:30:13 +0200

    ci: publish

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,4 +1,4 @@
 gradle applyPatches --stacktrace
 gradle build --stacktrace
 cp build/libs/*.jar /artifacts/paper.jar
-gradle publish --stacktrace -PpegasusPassword=$PEGASUS_PASSWORD
+gradle publish --stacktrace -PpegasusPassword=$1
